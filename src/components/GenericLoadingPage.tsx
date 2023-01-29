b54e62f66b4192ddd001ba6bd17f28c5c8f17915
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-29 10:55:06 +0200

    Created GenericLoadingPage.tsx

---
+++ src/components/GenericLoadingPage.tsx
@@ -0,0 +1,12 @@
+const GenericLoadingPage = () => {
+  return (
+    <div className="dashboard-page">
+      <div className="dashboard-container">
+        <div className="loading-animation" />
+      </div>
+    </div>
+  );
+};
+
+
+export default GenericLoadingPage
