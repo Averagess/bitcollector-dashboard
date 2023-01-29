0db48de28aa7d784a72c39e01db3e8f864ca7397
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-29 10:56:16 +0200

    added display for error msg

--- src/pages/PlayerEditor.tsx
+++ src/pages/PlayerEditor.tsx
@@ -12,8 +12,7 @@ const PlayerEditor = () => {
   const [search, setSearch] = useState("")
   const [player, setPlayer] = useState<null | Player>(null)
   const [notification, setNotification] = useState("")
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
+  const [error, setError] = useState(" ")
   const Navigate = useNavigate()
 
   useEffect(() => {
@@ -24,15 +23,16 @@ const PlayerEditor = () => {
     e.preventDefault()
     if(search.length === 0) return
 
-    setLoading(true)
     try {
       const player = await getPlayerById(search, token)
       setPlayer(player)
-      setLoading(false)
     } catch (error) {
       if(error instanceof AxiosError){
         if(!error.response) setError("No response from server")
-        else setError(error.response.data.message)
+        else {
+          setError(error.response.data.error)
+          setTimeout(() => setError(""), 3000)
+        }
       }
       else console.log("error", error)
     }
@@ -42,12 +42,12 @@ const PlayerEditor = () => {
     if(!player) return
 
     try {
-      const data = await updatePlayer(token, player)
+      await updatePlayer(token, player)
       setPlayer(null)
       setNotification("Player updated!")
       setTimeout(() => setNotification(""), 3000)
     } catch (error) {
-      console.log(error)
+      console.log("error happened trying to save player", error)
     }
   }
 
@@ -55,14 +55,15 @@ const PlayerEditor = () => {
     setPlayer(null)
   }
 
-
   return(
     <div className='dashboard-page'>
       <div className='dashboard-container' style={{position: "relative",display:"flex", alignItems: "center"}}>
         <h1>Player Editor</h1>
         {notification && <p style={{color: "green"}}>{notification}</p>}
+        <p style={{color: "red"}}>{error}</p>
+
         {!player && <form onSubmit={handleSubmit}>
-          <input onChange={(e) => setSearch(e.target.value)} value={search} placeholder="player id" />
+          <input onChange={(e) => setSearch(e.target.value)} value={search} placeholder="player id" className='login-input'/>
         </form>}
         {player && <PlayerProfile cancel={cancel} savePlayer={savePlayer} setPlayer={setPlayer} player={player} />}
         {!player && <button onClick={() => Navigate("/dashboard")} className="login-button" style={{position: "absolute", bottom: 0}}>back to dashboard</button>}
