3c2f9af4cd59523ef6779a5739b53c8a4627b9e7
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-31 11:31:45 +0200

    refactored onClick

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,14 +12,18 @@ const Dashboard = () => {
     if(token.length === 0) Navigate("/")
   },[])
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    Navigate(e.currentTarget.name)
+  }
+
   return(
     <div className="dashboard-page">
       <h1>Dashboard</h1>
       <div className="dashboard-container">
         <h1>What do you want to do?</h1>
         <div>
-          <button onClick={() => Navigate("/dashboard/player-editor")} className="login-button">edit player</button>
-          <button onClick={() => Navigate("/dashboard/all-players")}className="login-button">show all players</button>
+          <button name="/dashboard/all-players" onClick={handleClick}className="login-button">show all players</button>
+          <button name="/dashboard/player-editor"onClick={handleClick} className="login-button">edit player</button>
         </div>
       </div>
       <h1>{token}</h1>
