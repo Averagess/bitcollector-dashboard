de379e89a0aa7b3c839576fd1d90d4aa652b9efd
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-31 10:50:46 +0200

    Formatted code, added StatsContainer and Inventory

--- src/pages/PlayerProfile.tsx
+++ src/pages/PlayerProfile.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import GenericLoadingPage from "../components/GenericLoadingPage";
+import StatsCotainer from "../components/StatsContainer";
+import InventoryContainer from "../components/InventoryContainer";
 import AuthContext from "../contexts/AuthContext";
 import { getPlayerById } from "../services/playerService";
 import { Player } from "../types";
@@ -9,37 +11,54 @@ const PlayerProfile = () => {
   const [player, setPlayer] = useState<Player | null>(null);
   const { token } = useContext(AuthContext);
   const { id } = useParams<{ id: string }>();
-  const [ loading, setLoading] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true);
   const Navigate = useNavigate();
 
   useEffect(() => {
     if (token.length === 0) return Navigate("/");
     if (!id) return Navigate("/dashboard/all-players");
-    
-    const fetchPlayerData = async() => {
+
+    const fetchPlayerData = async () => {
       try {
         const player = await getPlayerById(id, token);
         setPlayer(player);
-        setLoading(false)
+        setLoading(false);
       } catch (error) {
         console.log("error", error);
       }
-
-    }
+    };
 
     fetchPlayerData();
-  }, [])
+  }, []);
 
-
-
-  if(loading || !player) return <GenericLoadingPage />
+  if (loading || !player) return <GenericLoadingPage />;
 
   return (
     <div className="dashboard-page">
-      <div className="dashboard-container">
-        <h1 style={{margin: 0}}>{player.discordDisplayName}</h1>
-        <h3 style={{margin: 0}}>{player.discordId}</h3>
-        
+      <div
+        className="dashboard-container"
+        style={{
+          width: "80vw",
+          height: "60vh",
+          borderRadius: 25,
+          overflow: "auto",
+        }}
+      >
+        <h1 style={{ margin: 0 }}>{player.discordDisplayName}</h1>
+        <h3 style={{ margin: 0 }}>{player.discordId}</h3>
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ margin: 15, width: "100%" }}>
+            <h1 style={{ textDecoration: "underline" }}>Stats</h1>
+            <StatsCotainer player={player} />
+          </div>
+
+          <div style={{ margin: 15, width: "100%" }}>
+            <h1 style={{ textDecoration: "underline" }}>Inventory</h1>
+            <InventoryContainer inventory={player.inventory} />
+          </div>
+        </div>
+        <button className="login-button" onClick={() => Navigate("/dashboard/all-players")}>Back to player list</button>
       </div>
     </div>
   );
