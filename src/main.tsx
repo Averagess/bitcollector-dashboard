7b62390bbeac4ac7783055401dea6cd17fdc818c
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-08 16:45:45 +0200

    removed dead code

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import Login from './pages/Login'
 import "./main.css"
 
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import Dashboard from './pages/Dashboard'
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Login />
-//   },
-//   {
-//     path: "/dashboard",
-//     element: <Dashboard />
-//   }
-// ])
+console.log(`APP RUNNING IN ${import.meta.env.MODE} MODE`)
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
