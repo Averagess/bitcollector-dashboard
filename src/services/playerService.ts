07bd199ea54e2450b5ea889098774f7dec5546e2
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-08 16:48:43 +0200

    formatted

--- src/services/playerService.ts
+++ src/services/playerService.ts
@@ -1,35 +1,40 @@
-import axios from 'axios';
-import { Player } from '../types';
-const API_URL = 'http://localhost:3000/api'
+import axios from "axios";
+import { Player } from "../types";
 
-
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "http://3.73.209.127:8080/api";
 
 export const getAllPlayers = async (token: string) => {
   const { data } = await axios.get(`${API_URL}/allPlayers`, {
     headers: {
-      Authorization: `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
-  
-  return data
-}
+
+  return data;
+};
 
 export const getPlayerById = async (id: string, token: string) => {
-  const body = { discordId: id }
+  const body = { discordId: id };
   const headers = {
-    Authorization: `Bearer ${token}`
-  }
+    Authorization: `Bearer ${token}`,
+  };
 
-
-  const { data } = await axios.post<Player>(`${API_URL}/updatePlayer`, body, { headers })
-  return data
-}
+  const { data } = await axios.post<Player>(`${API_URL}/updatePlayer`, body, {
+    headers,
+  });
+  return data;
+};
 
 export const updatePlayer = async (token: string, body: Player) => {
   const headers = {
-    Authorization: `Bearer ${token}`
-  }
+    Authorization: `Bearer ${token}`,
+  };
 
-  const { data } = await axios.put(`${API_URL}/updatePlayer`, body, { headers })
-  return data
-}
+  const { data } = await axios.put(`${API_URL}/updatePlayer`, body, {
+    headers,
+  });
+  return data;
+};
