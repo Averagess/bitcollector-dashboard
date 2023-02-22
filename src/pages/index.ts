5b451eb75188ce430bc51a256fbb76c7324f1dee
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-22 05:04:51 +0200

    created index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,15 @@
+import NotFoundPage from "./404";
+import AllPlayers from "./AllPlayers";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import PlayerEditor from "./PlayerEditor";
+import PlayerProfile from "./PlayerProfile";
+
+export {
+  NotFoundPage,
+  AllPlayers,
+  Dashboard,
+  Login,
+  PlayerEditor,
+  PlayerProfile,
+};
