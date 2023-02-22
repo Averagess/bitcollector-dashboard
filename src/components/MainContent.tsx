2b3841786153cf87b26dc4e3c73e1d9244c00c9e
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-22 12:05:20 +0200

    changed bg color

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 const MainContent = ({ children, classes }: Props) => {
   return(
-    <div className={`container flex flex-col p-12 text-slate-200 bg-zinc-800 rounded-xl font-arial border-2 border-red-500 ${classes}`}>
+    <div className={`container flex flex-col p-12 text-slate-200 bg-zinc-900 rounded-xl font-arial border-2 border-red-500 ${classes}`}>
       {children}
     </div>
   )
