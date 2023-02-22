f4256f6c5b883ff784841f19cd9d0147d5c090e1
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-22 05:06:47 +0200

    centered

--- src/components/GenericLoadingPage.tsx
+++ src/components/GenericLoadingPage.tsx
@@ -4,7 +4,7 @@ import PageContainer from "./PageContainer";
 const GenericLoadingPage = () => {
   return (
     <PageContainer>
-      <MainContent>
+      <MainContent classes="my-auto">
         <div className="m-auto w-80 h-80 border-sky-500 border-4 border-l-white rounded-full animate-spin-slow" />
       </MainContent>
     </PageContainer>
