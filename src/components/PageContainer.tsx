a419d0d28ff0332c249188162d1fe0cb80cce956
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-22 05:04:32 +0200

    Created PageContainer And MainContent

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children? : React.ReactNode;
+}
+
+const PageContainer = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col h-full w-full items-center">
+      {children}
+    </div>
+  );
+};
+
+export default PageContainer;
