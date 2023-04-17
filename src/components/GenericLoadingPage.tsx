a6722d404f7953d93ef193a23cb2cc74e5a4f746
Averagess <45748163+Averagess@users.noreply.github.com>
2023-04-17 20:53:52 +0300

    Added NavBar to loading page

--- src/components/GenericLoadingPage.tsx
+++ src/components/GenericLoadingPage.tsx
@@ -1,9 +1,11 @@
 import MainContent from "./MainContent";
+import NavBar from "./NavBar";
 import PageContainer from "./PageContainer";
 
 const GenericLoadingPage = () => {
   return (
     <PageContainer>
+      <NavBar />
       <MainContent classes="my-auto">
         <div className="m-auto w-80 h-80 border-sky-500 border-4 border-l-white rounded-full animate-spin-slow" />
       </MainContent>
