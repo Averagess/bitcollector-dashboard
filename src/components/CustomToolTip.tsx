f6d1f401c2963c62f40880c331e083dc1f919976
Averagess <45748163+Averagess@users.noreply.github.com>
2023-04-20 16:30:16 +0300

    made customtooltip output in readable format

--- src/components/CustomToolTip.tsx
+++ src/components/CustomToolTip.tsx
@@ -1,13 +1,18 @@
+import readableNumber from "../helpers/readableNumber";
+
 const CustomToolTip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
     return (
-      <div className="text-purple-500 bg-black p-2 border-2 bg-opacity-50 rounded-xl">
+      <div className="text-cyan-500 bg-black p-2 border-2 bg-opacity-50 rounded-xl">
         <h1>{label}</h1>
         {payload.map((item: any, i: number) => {
           return (
             <div key={i}>
-              <h1>{item.name}</h1>
-              <h1>{item.value}</h1>
+              {typeof item.value === "number" ? (
+                <h1>{readableNumber(item.value.toString())}</h1>
+              ) : (
+                <h1>{item.value}</h1>
+              )}
             </div>
           );
         })}
